4bbd1498d61b98397d4c10a2db7a30faff08c7d7
EC2 Default User <ec2-user@ip-172-31-27-40.us-east-2.compute.internal>
2021-10-04 14:23:16 +0000

    Elijah code challenge 10/4/2021

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 //1
-function countToTen(){
-  
+function isAllLowerCase(string){
+  const lowercase = string.toLowerCase()
+  if(lowercase === string){
+      return true
+  }else{
+      return false
+  }
 }
+console.log(isAllLowerCase("HEY"))
 
 //2
-function countFromOne(){
-  
-}
+function countFromOne(int){
+  for(let i = 1; i <= int; i++);{ 
+console.log(i);
+    }
+  }
+countFromOne(15)
 
 //3
-function countEveryEven(){
-  
-}
+function countEveryEven(number){
+  for(let i = 0; i <= number; i += 2);
+} console.log(18);
 
 //4
-function countEveryOdd(){
+function countEveryOdd(number){
+    for(let i = 1; i < number; i++){
+        if(i % 2 !== 0){
+            console.log(i)
+        }
+    }
   
 }
+countEveryOdd(8)
 
-//5 
-function countEvens(){
-  
+function isNegative(number){
+    if(number < 0){
+        return true
+    }else{
+        return false
+    }
 }
+console.log(isNegative(-4))
+
+function betweenFiveAndTwenty(num){
+    if(num >= 5 && num <= 20){
+        return true
+    }else{ 
+        return false
+    }
+}
+console.log(betweenFiveAndTwenty(100))
 
